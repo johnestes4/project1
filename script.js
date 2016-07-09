27cc45ffff4c9b4d6610c9fd611f8122f633e0a6
johnestes4 <johnestes4@gmail.com>
2016-07-09 19:37:42 -0400

    Added basic color changing, experimenting with keeping track of turns

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
   console.log("Good to go!")
+  var gameOn = false;
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+  $('.simon').click(function(){
+
+    if (gameOn == true) {
+      var newColor = $(this).attr('id');
+      console.log(newColor);
+      $(this).css('background-color', newColor);
+    }
+
+  });
+
+  $('#begin').click(function(){
+    alert('START');
+    gameOn = true;
+    var turn = 1;
+    var quadrantsLit = 0;
+    setInterval(function(){
+      if (gameOn == true && quadrantsLit < turn); {
+
+        var whichQuadrant = getRandomInt(0,3);
+        console.log(whichQuadrant);
+        if (whichQuadrant == 0) {
+          $(green).css('background-color', 'green');
+        }
+        else if (whichQuadrant == 1) {
+          $(red).css('background-color', 'red');
+        }
+        else if (whichQuadrant == 2) {
+          $(blue).css('background-color', 'blue');
+        }
+        else if (whichQuadrant == 3) {
+          $(yellow).css('background-color', 'yellow');
+        }
+      }
+    }, 1000);
+  });
 });
---
+++ userstories.md
@@ -0,0 +1,10 @@
+
+As a user, I want to see the quadrants light up, so I know where to click.
+
+I want the quadrants to light up randomly as I click them, so the game will pose a challenge.
+
+I want the game to know whether I clicked properly, so I know if I won or lost.
+
+I want the game to keep track of my score, so I know how well I did.
+
+I want the game to reset when I miss a click, so that I have to try not to lose.
